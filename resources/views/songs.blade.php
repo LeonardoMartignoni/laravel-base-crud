879ce3ff404defa0b9c6991440107783b4f90bd9
LeonardoMartignoni <leonardo.martignonii@gmail.com>
2023-04-03 15:55:31 +0200

    Added table for songs list

--- resources/views/songs.blade.php
+++ resources/views/songs.blade.php
@@ -5,7 +5,32 @@
 @endsection
 
 @section('main-content')
+  {{-- @dump($songs) --}}
+
   <div class="container">
-    <h1>Songs list</h1>
+    <h1 class="my-2">Songs list</h1>
+    <table class="table table-striped">
+      <thead>
+        <tr>
+          <th scope="col">id</th>
+          <th scope="col">title</th>
+          <th scope="col">album</th>
+          <th scope="col">author</th>
+          <th scope="col">editor</th>
+          <th scope="col">length</th>
+        </tr>
+      </thead>
+      <tbody>
+        @foreach ($songs as $song)
+          <tr>
+            <th scope="row">{{ $song['id'] }}</th>
+            <td>{{ $song['title'] }}</td>
+            <td>{{ $song['album'] }}</td>
+            <td>{{ $song['author'] }}</td>
+            <td>{{ $song['editor'] }}</td>
+            <td>{{ number_format($song['length'], 2) }}</td>
+          </tr>
+        @endforeach
+    </table>
   </div>
 @endsection
