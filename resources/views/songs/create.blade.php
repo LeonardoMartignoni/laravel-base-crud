989393e73e63a74fcadd9930a0c2e638f2b3110b
LeonardoMartignoni <leonardo.martignonii@gmail.com>
2023-04-03 16:45:59 +0200

    Refactor validator

--- resources/views/songs/create.blade.php
+++ resources/views/songs/create.blade.php
@@ -24,27 +24,28 @@
       <div class="row g-3">
         <div class="col-6">
           <label for="title" class="form-label">Title</label>
-          <input type="text" name="title" class="form-control">
+          <input type="text" name="title" class="form-control @error('title') border border-danger @enderror">
         </div>
         <div class="col-6">
           <label for="album" class="form-label">Album</label>
-          <input type="text" name="album" class="form-control">
+          <input type="text" name="album" class="form-control @error('album') border border-danger @enderror">
         </div>
         <div class="col-6">
           <label for="author" class="form-label">Author</label>
-          <input type="text" name="author" class="form-control">
+          <input type="text" name="author" class="form-control @error('author') border border-danger @enderror">
         </div>
         <div class="col-6">
           <label for="editor" class="form-label">Editor</label>
-          <input type="text" name="editor" class="form-control">
+          <input type="text" name="editor" class="form-control @error('editor') border border-danger @enderror">
         </div>
         <div class="col-6">
           <label for="length" class="form-label">Length</label>
-          <input type="number" step="0.01" name="length" class="form-control">
+          <input type="number" step="0.01" name="length"
+            class="form-control @error('length') border border-danger @enderror">
         </div>
         <div class="col-6">
           <label for="poster" class="form-label">Poster</label>
-          <input type="text" name="poster" class="form-control">
+          <input type="text" name="poster" class="form-control @error('poster') border border-danger @enderror">
         </div>
         <div class="col-12">
           <button type="submit" class="d-block ms-auto btn btn-outline-dark">Create</button>
