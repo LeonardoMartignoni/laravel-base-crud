4e02ce4b6d6315f3dd2ea58397040bd2c5b5a5f5
LeonardoMartignoni <leonardo.martignonii@gmail.com>
2023-04-04 16:20:09 +0200

    Refactor songs.edit

--- resources/views/songs/edit.blade.php
+++ resources/views/songs/edit.blade.php
@@ -25,28 +25,33 @@
       <div class="row g-3">
         <div class="col-6">
           <label for="title" class="form-label">Title</label>
-          <input type="text" name="title" class="form-control @error('title') border border-danger @enderror">
+          <input value="{{ old('title') ?? $song->title }}" type="text" name="title"
+            class="form-control @error('title') border border-danger @enderror">
         </div>
         <div class="col-6">
           <label for="album" class="form-label">Album</label>
-          <input type="text" name="album" class="form-control @error('album') border border-danger @enderror">
+          <input value="{{ old('album') ?? $song->album }}" type="text" name="album"
+            class="form-control @error('album') border border-danger @enderror">
         </div>
         <div class="col-6">
           <label for="author" class="form-label">Author</label>
-          <input type="text" name="author" class="form-control @error('author') border border-danger @enderror">
+          <input value="{{ old('author') ?? $song->author }}" type="text" name="author"
+            class="form-control @error('author') border border-danger @enderror">
         </div>
         <div class="col-6">
           <label for="editor" class="form-label">Editor</label>
-          <input type="text" name="editor" class="form-control @error('editor') border border-danger @enderror">
+          <input value="{{ old('editor') ?? $song->editor }}" type="text" name="editor"
+            class="form-control @error('editor') border border-danger @enderror">
         </div>
         <div class="col-6">
           <label for="length" class="form-label">Length</label>
-          <input type="number" step="0.01" name="length"
+          <input value="{{ old('length') ?? $song->length }}" type="number" step="0.01" name="length"
             class="form-control @error('length') border border-danger @enderror">
         </div>
         <div class="col-6">
           <label for="poster" class="form-label">Poster</label>
-          <input type="text" name="poster" class="form-control @error('poster') border border-danger @enderror">
+          <input value="{{ old('poster') ?? $song->poster }}" type="text" name="poster"
+            class="form-control @error('poster') border border-danger @enderror">
         </div>
         <div class="col-12">
           <button type="submit" class="d-block ms-auto btn btn-outline-dark">Save Changes</button>
